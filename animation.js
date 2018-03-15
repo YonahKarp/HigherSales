3f8871801f9165840b136004cc54711486f82e90
Yonah Karp <ykarp@student.touro.edu>
2018-03-14 22:25:21 -0400

    lightslider load fix

--- animation.js
+++ animation.js
@@ -65,74 +65,72 @@ $(document).ready(function(){
         
     })
 
-    $("#clients").lightSlider({
-        item: 3,
-        autoWidth: true,
-        slideMove: 1, // slidemove will be 1 if loop is true
-        slideMargin: 60,
- 
-        addClass: '',
-        mode: "slide",
-        useCSS: true,
-        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-        easing: 'ease-in-out', //'for jquery animation',////
- 
-        speed: 1200, //ms'
-        auto: true,
-        loop: true,
-        slideEndAnimation: false,
-        pause: 3000,
- 
-        pager: true,
-        currentPagerPosition: 'middle',
- 
-        enableTouch:true,
-        enableDrag:true,
-        freeMove:true,
-        swipeThreshold: 40,
- 
-        responsive : [
-            
-        ]
+    $(window).on("load", function() {
+        $("#clients").lightSlider({
+            item: 3,
+            autoWidth: true,
+            slideMove: 1, // slidemove will be 1 if loop is true
+            slideMargin: 60,
+     
+            addClass: '',
+            mode: "slide",
+            useCSS: true,
+            cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+            easing: 'ease-in-out', //'for jquery animation',////
+     
+            speed: 1200, //ms'
+            auto: true,
+            loop: true,
+            slideEndAnimation: false,
+            pause: 3000,
+     
+            pager: true,
+            currentPagerPosition: 'middle',
+     
+            enableTouch:true,
+            enableDrag:true,
+            freeMove:true,
+            swipeThreshold: 40,
+     
+            responsive : [
+                
+            ]
+        }); 
     });
-
 
     //contactUS button
     $("#contactUs .rplButton").click(function(e){
-        
+    
         var _this = $(this)
         
         var x = (e.pageX - _this.offset().left)  -15;
         var y = (e.pageY - _this.offset().top)   -15;
         
-         $(this).next().css({
+            $(this).next().css({
             backgroundColor: "rgba(63, 175, 239, 0.8)", 
             left: x, 
             top: y, 
             height: "30px", 
             width:"30px"});
         $(this).next().animate({
-          backgroundColor: "rgba(63, 175, 239,0.15)",  
-          height: "+=400", 
-          width:"+=400",
-          top: "-=200", 
-          left: "-=200"
-         },
-         {
-           duration: 400,
-           complete: function(){
-             $(this).css({
-              backgroundColor: "rgb(32,178,0)", 
-              height: "0px", 
-              width:"0px"});
-              
-              
-              //document.forms[0].submit();
-              //message sent!
-           }
-          })
-      });
-
-
-
+            backgroundColor: "rgba(63, 175, 239,0.15)",  
+            height: "+=400", 
+            width:"+=400",
+            top: "-=200", 
+            left: "-=200"
+            },
+            {
+            duration: 400,
+            complete: function(){
+                $(this).css({
+                backgroundColor: "rgb(32,178,0)", 
+                height: "0px", 
+                width:"0px"});
+                
+                
+                //document.forms[0].submit();
+                //message sent!
+            }
+            })
+        });   
 })
